70e418c492c7e2c33fb1a174686232efd3c75d5e
sejal <sejalsingh417@gmail.com>
2021-08-21 16:22:00 +0530

    day19

---
+++ Day-19.cpp
@@ -0,0 +1,53 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic
+{
+public:
+    virtual int divisorSum(int n) = 0;
+};
+class Calculator : public AdvancedArithmetic
+{
+
+public:
+    virtual int divisorSum(int n) override
+    {
+        int sum = 0;
+        if (1 == n)
+        {
+            return 1;
+        }
+
+        for (int lo = 1, hi = n; lo < hi; ++lo)
+        {
+            if (n % lo)
+            {
+                continue;
+            }
+            sum += lo;
+            hi = (n / lo);
+            if (hi <= lo)
+            {
+                break;
+            }
+            sum += hi;
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator();
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n"
+         << sum;
+    return 0;
+}
